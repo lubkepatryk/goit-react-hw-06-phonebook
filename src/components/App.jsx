de7183c2edbbb4dd1676a844ba56cae52fc2fb14
lubkepatryk <lubke.patryk@gmail.com>
2023-07-10 19:33:01 +0200

    deleted not used line

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
